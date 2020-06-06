ef0a3cd8f9c26f92f4209d49cf9de9b1b1374aa7
Sudarshan Peruru <peruru.sudarshan@gmail.com>
2020-06-06 11:01:04 +0530

    Initial Commit

---
+++ src/main/java/com/springproject/expproject/ExpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.expproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpProjectApplication.class, args);
+	}
+
+}
