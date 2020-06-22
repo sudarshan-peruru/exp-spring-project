aa0e673594beb04a60e2601c29c8e83173078eed
Sudarshan Peruru <peruru.sudarshan@gmail.com>
2020-06-22 22:42:28 +0530

    JsonIgnore - First Commit

--- src/main/java/com/springproject/expproject/entities/User.java
+++ src/main/java/com/springproject/expproject/entities/User.java
@@ -11,8 +11,12 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "user")
+@JsonIgnoreProperties({"firstname", "lastname"})
 public class User {
 	@Id 
 	@GeneratedValue
@@ -37,6 +41,7 @@ public class User {
 	private String role;
 	
 	@Column(name="SSN", length=50, nullable=false, unique=true)
+	@JsonIgnore
 	private String ssn;
 	
 	@OneToMany(mappedBy="user")
